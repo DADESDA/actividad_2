73423aa717696c671e2674ab34c92d4cad4688c6
esmeralda <esmeralda8cespedes@gmail.com>
2026-06-11 20:11:04 -0400

    cambios realizados en index hatml

--- servidor.py
+++ servidor.py
@@ -1,21 +1,20 @@
-# servidor.py - Servidor básico para servir la página web
 
 import http.server
 import socketserver
 import os
 
-# Puerto donde correrá el servidor
+
 PUERTO = 8000
 
-# Cambiar al directorio donde están los archivos
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# Configurar el servidor
+
 Handler = http.server.SimpleHTTPRequestHandler
 
 print(f"🍽️  Servidor FoodOrder corriendo en: http://localhost:{PUERTO}")
 print("Para detenerlo presiona Ctrl + C")
 
-# Iniciar el servidor
+
 with socketserver.TCPServer(("", PUERTO), Handler) as httpd:
     httpd.serve_forever()
